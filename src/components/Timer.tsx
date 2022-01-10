39e82ae27dab4cf67f7811d1022aa9515d8fcd4e
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-10 10:27:13 +0100

    Add time left component

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,9 +1,14 @@
-import { TimerLabelStyled, TimerStyled } from "../styles/TimerStyled";
+import {
+  TimeLeftStyled,
+  TimerLabelStyled,
+  TimerStyled
+} from "../styles/TimerStyled";
 
 const Timer = () => {
   return (
     <TimerStyled>
       <TimerLabelStyled id="timer-label">Session</TimerLabelStyled>
+      <TimeLeftStyled id="time-left">25:00</TimeLeftStyled>
     </TimerStyled>
   );
 };
