01bb0923cf2a831de0fa6b80d941a35efe4a0632
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-10 18:18:09 +0100

    Add start-stop button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
+import { Pause, Play } from "@styled-icons/boxicons-regular";
 import { useState } from "react";
+import Button from "./components/Button";
 import Heading from "./components/Heading";
 import SettingsContainer from "./components/SettingsContainer";
 import Timer from "./components/Timer";
 import AppStyled from "./styles/AppStyled";
+import ButtonsContainer from "./styles/ButtonContainer";
 
 const App = () => {
   const [breakLength, setBreakLength] = useState(5);
   const [sessionLength, setSessionLength] = useState(25);
   const [isRunning, setIsRunnig] = useState(false);
+
+  const iconSize = 48;
 
   const timerControl = () => {
     setIsRunnig(!isRunning);
@@ -24,6 +29,15 @@ const App = () => {
         setSessionLength={setSessionLength}
       />
       <Timer />
+      <ButtonsContainer>
+        <Button id="start_stop">
+          {isRunning ? (
+            <Pause onClick={timerControl} size={iconSize} />
+          ) : (
+            <Play onClick={timerControl} size={iconSize} />
+          )}
+        </Button>
+      </ButtonsContainer>
     </>
   );
 };
