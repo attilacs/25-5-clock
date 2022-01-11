5fb5972dd29f216daec054398731e6fe9caf9530
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-11 12:34:36 +0100

    Create decrement length function

--- src/components/service.ts
+++ src/components/service.ts
@@ -8,3 +8,14 @@ export const incrementLength = (
     setter(value + 1);
   }
 };
+
+export const decrementLength = (
+  value: number,
+  setter: React.Dispatch<React.SetStateAction<number>>,
+  isRunning: boolean
+) => {
+  const minLength = 1;
+  if (!isRunning && minLength < value) {
+    setter(value - 1);
+  }
+};
