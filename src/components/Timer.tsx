b1f959e747460b4bb068eca64f33fe730c26e712
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-13 07:56:45 +0100

    Refactor display clock function

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -11,9 +11,11 @@ interface TimerProps {
 
 const Timer = ({ secondsLeft, mode }: TimerProps) => {
   const displayClock = (seconds: number) => {
-    const min = Math.floor(seconds / 60);
-    const sec = seconds % 60;
-    return `${min < 10 ? "0" + min : min}:${sec < 10 ? "0" + sec : sec}`;
+    const min = Math.floor(seconds / 60)
+      .toString()
+      .padStart(2, "0");
+    const sec = (seconds % 60).toString().padStart(2, "0");
+    return `${min}:${sec}`;
   };
 
   const label = mode === "session" ? "Session" : "Break";
