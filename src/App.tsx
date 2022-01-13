dd2ab19eef52254a76967d2fee05af5ccdce684c
Attila Csanyi <csanyiattila23@gmail.com>
2022-01-13 09:28:24 +0100

    Remove redundant parameter from display clock function

--- src/App.tsx
+++ src/App.tsx
@@ -116,11 +116,11 @@ const App = () => {
     }
   };
 
-  const displayClock = (seconds: number) => {
-    const min = Math.floor(seconds / 60)
+  const displayClock = () => {
+    const min = Math.floor(timeLeft / 60)
       .toString()
       .padStart(2, "0");
-    const sec = (seconds % 60).toString().padStart(2, "0");
+    const sec = (timeLeft % 60).toString().padStart(2, "0");
     return `${min}:${sec}`;
   };
 
